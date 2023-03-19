eb3c95f53130548780d99472635f683466299b73
clearly mine <clearlymine@pm.me>
2023-03-19 16:43:06 +0530

    feat: add NFT contract for Clearly Mine(CM)

---
+++ src/NFT.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721 {
+    uint256 public currentTokenId;
+
+    constructor() ERC721("Clearly Mine", "CM") {}
+
+    function mint(address recipient) public payable returns (uint256) {
+        uint256 newItemId = ++currentTokenId;
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
+}
